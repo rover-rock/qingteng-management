12a4bdfbf4f23453877d207409957f8bbaceb045
rover-rock <362463215@qq.com>
2020-03-12 14:05:06 +0800

    '111'

---
+++ src/view/single-page/home/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+  data () {
+    return {
+
+    }
+  },
+  mounted () {
+    //
+  }
+}
+</script>
+
+<style lang="less">
+
+</style>
